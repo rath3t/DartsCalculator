13bb92538189870688a848dbedd6ba6ad7699c80
AlexanderMueller <rathet@gmail.com>
2024-02-14 14:40:14 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 
 
-use ferris_says::say;
+use circular_buffer::CircularBuffer;
 use std::io::{BufWriter, stdout};
 
+
 fn main() {
+    let mut buf = CircularBuffer::<5, u32>::new();
+
     let testString = b"Hello2";
+
     let width =24;
     let mut writer = BufWriter::new(stdout());
 
