d4fcd4a418535f1a60f0c123d12a4203d958dbe7
AlexanderMueller <rathet@gmail.com>
2024-02-17 09:34:34 +0100

    more refactor

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-use std::iter::Enumerate;
-use circular_buffer::CircularBuffer;
 use std::fmt;
 use std::mem::swap;
 use std::collections::HashMap;
-use once_cell::sync::Lazy;
- const fn dividableBy(i: u32 ) -> u32
+use std::fmt::{Display, Formatter};
+
+ const fn dividable_by(i: u32 ) -> u32
 {
     let mut count = 0;
     if i==0
@@ -34,7 +33,7 @@ struct Dart
 
 }
 
-#[derive(Debug)]
+#[derive(Debug,Copy, Clone)]
 
 struct Neighboors(u32,u32);
 
@@ -48,16 +47,9 @@ struct Complexity
 }
 
 const SINGLE: [u32;21] = [1,2,3,4,5,6,7,8,9,10, 11,12,13,14,15,16,17,18,19,20,25];
-const BOARD: [u32;20] = [20,1,18,4,13,6,10,15,2,17,3,19,7,16,8,11,14,9,12,5];
+//const BOARD: [u32;20] = [20,1,18,4,13,6,10,15,2,17,3,19,7,16,8,11,14,9,12,5];
 
-use phf::{Map, phf_map};
-
-
-static map: Map<&str, &str> = phf_map! {
-  "index.html" => ("../dist/index.html"),
-  "assets/script-44b5bae5.js" => ("../dist/assets/script-44b5bae5.js"),
-  "assets/style-48a8825f.css" => ("../dist/assets/style-48a8825f.css")
-};
+use phf::{phf_map};
 
 static NEIGHBOORS: phf::Map<u32, Neighboors> = phf_map! {
     1u32 => Neighboors(20,18),
@@ -76,6 +68,11 @@ static NEIGHBOORS: phf::Map<u32, Neighboors> = phf_map! {
     14u32 => Neighboors(11,9),
     15u32 => Neighboors(10,2),
     16u32 => Neighboors(7,8),
+    17u32 => Neighboors(2,3),
+    18u32 => Neighboors(1,4),
+    19u32 => Neighboors(3,7),
+    20u32 => Neighboors(5,1),
+    25u32 => Neighboors(0,0),
         };
 
 #[derive(PartialEq, Eq, PartialOrd, Ord)]
@@ -86,6 +83,44 @@ struct Checkout {
 
 
 }
+impl Display for Neighboors {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{:>2}, {:>2}", self.0, self.1)
+    }
+}
+
+impl Display for Complexity {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let mut str= String::from("Neighboors: ");
+        for neighboor in &self.neighboors{
+             str += &*format!(" {{{}}}, ", neighboor);
+
+        }
+
+        write!(f, "Darts: {}, Last dart halfability: {}, {}",
+                   self.darts, self.halfable, str)
+
+    }
+}
+impl Display for Checkout {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let multiplier_string = |i: u32| -> &str { if i==1 {"S"}else if i==2 {"D"} else {"T"}};
+
+        if self.first_dart.multiplier > 0 && self.second_dart.multiplier > 0
+        {
+            write!(f, "match {}{: >2} + {}{: >2} + D{: >2} -> Score {}",
+                   multiplier_string(self.first_dart.multiplier), self.first_dart.value, multiplier_string(self.second_dart.multiplier), self.second_dart.value, self.third_dart.value, self.complexity())
+        } else if self.first_dart.multiplier == 0 && self.second_dart.multiplier > 0
+        {
+            write!(f, "match      {}{: >2} + D{: >2} -> Score {}",
+                   multiplier_string(self.second_dart.multiplier), self.second_dart.value, self.third_dart.value, self.complexity())
+        } else {
+            write!(f, "match            D{: >2} -> Score {}",
+                   self.third_dart.value, self.complexity())
+        }
+    }
+}
+
 impl Checkout
 {
     fn complexity(&self) -> Complexity
@@ -93,31 +128,25 @@ impl Checkout
         let first_val = self.first_dart.value;
         let second_val = self.second_dart.value;
         let third_val = self.third_dart.value;
-        let halfableV = HALFABLE[if (third_val as usize) <25 { third_val as usize -1} else { 20}];
+        let halfable_v = HALFABLE[if (third_val as usize) <25 { third_val as usize -1} else { 20}];
         let mut count =3;
         if self.first_dart.multiplier==0
         { count -= 1; }
         if self.second_dart.multiplier==0
         { count -= 1; }
 
-        let  firstleftneighboor = if first_val== *BOARD.first().unwrap() { *BOARD.last().unwrap() } else {*BOARD.get(first_val as usize-1).unwrap()};
-        let  firstrightneighboor = if first_val== *BOARD.last().unwrap() { *BOARD.first().unwrap() } else {*BOARD.get(first_val as usize+1).unwrap()};
+        let n1 :& Neighboors = &NEIGHBOORS[&first_val];
+        let n2 : &Neighboors = &NEIGHBOORS[&second_val];
+        let n3 : &Neighboors = &NEIGHBOORS[&third_val];
 
-        let n1 : Neighboors = Neighboors(firstleftneighboor, firstrightneighboor);
-
-        let  secondleftneighboor = if second_val== *BOARD.first().unwrap() { *BOARD.last().unwrap() } else {*BOARD.get(second_val as usize-1).unwrap()};
-        let  secondrightneighboor = if second_val== *BOARD.last().unwrap() { *BOARD.first().unwrap() } else {*BOARD.get(second_val as usize+1).unwrap()};
-        let n2 : Neighboors = Neighboors(secondleftneighboor, secondrightneighboor);
-
-        let  thirdleftneighboor = if third_val== *BOARD.first().unwrap() { *BOARD.last().unwrap() } else {*BOARD.get(third_val as usize-1).unwrap()};
-        let  thirdrightneighboor = if third_val== *BOARD.last().unwrap() { *BOARD.first().unwrap() } else {*BOARD.get(third_val as usize+1).unwrap()};
-        let n3 : Neighboors = Neighboors(thirdleftneighboor, thirdrightneighboor);
        return  Complexity{
-           halfable: halfableV,
+           halfable: halfable_v,
            darts: count,
-           neighboors: if count==1 { vec![n3] } else if count==2 {vec![n2,n3]}else {vec![n1,n2,n3]}
+           neighboors: if count==1 { vec![*n3] } else if count==2 {vec![*n2,*n3]}else {vec![*n1,*n2,*n3]}
        };
     }
+
+
 }
 
 
@@ -127,7 +156,7 @@ impl Checkout
     let mut i=0;
     let mut array_half_able: [u32;COUNT] = [0;COUNT];
     while i<COUNT { // needed a while loop since iteration is not const in rust https://github.com/rust-lang/rfcs/blob/master/text/2344-const-looping.md
-        array_half_able[i] = dividableBy(array[i]);
+        array_half_able[i] = dividable_by(array[i]);
         i+=1;
     }
     return array_half_able;
@@ -135,58 +164,13 @@ impl Checkout
 
 const COUNT: usize = 21;
 
-const HALFABLE: [u32;21] = create_half_able(&SINGLE);
-
-// const HALFABLEMAP: phf::Map::<usize,u32> = phf_map!{
-//     1 => 0,
-//     2 => 1,
-//     3 => 0,
-//     4 => 2,
-//     5 => 0,
-//     6 => 1,
-//     7 => 0,
-//     8 => 3,
-//     9 => 0,
-//     10 => 1,
-//     11 => 0,
-//     12 => 2,
-//     13 => 0,
-//     14 => 1,
-//     15 => 0,
-//     16 => 4,
-//     };
-
-// const fn createHalfAbleMap() -> phf::Map::<usize,u32>
-// {
-//     let mut halfablemap = phf::Map::<usize,u32>::new();
-//     let mut i=0;
-//
-//     while i<COUNT  {
-//         if i==20 { halfablemap.(25,0); continue}
-//         halfablemap.insert(i+1,HALFABLEMAP[&i]);
-//         i+=1;
-//     }
-//     return halfablemap;
-// }
-
-
-
-
+const HALFABLE: [u32;COUNT] = create_half_able(&SINGLE);
 
 fn main() {
 
-    let arrayWithoutBull: [u32;20] = [1,2,3,4,5,6,7,8,9,10, 11,12,13,14,15,16,17,18,19,20];
-
-    for (i,h) in HALFABLE.iter().enumerate() {
-        let str = format!("{:?} is {} dividable by 2", SINGLE[i], h);
-        println!("{}", str.as_str() )
-    }
-    let board = CircularBuffer::<21, u32>::from(BOARD);
     let mut checkouts: Vec<Checkout> = Vec::<Checkout>::new();
 
-
-
-    let test_number = 6;
+    let test_number = 121;
     for first_dart_mult in [0,1,2,3] {
         for first_dart_value in SINGLE {
             if first_dart_mult == 0 && first_dart_value > 1 {continue}
@@ -205,8 +189,6 @@ fn main() {
                     let third_dart = 2 * third_dart_value;
 
                     if test_number == first_dart + second_dart + third_dart {
-                        let test_number_reproduction =first_dart_mult*first_dart_value+ second_dart_mult*second_dart_value+ 2*third_dart_value;
-                        //println!("match {first_dart_mult}*{first_dart_value}+ {second_dart_mult}*{second_dart_value}+ 2*{third_dart_value} = {test_number_reproduction}");
                         checkouts.push(Checkout{ first_dart: Dart { value: first_dart_value, multiplier: first_dart_mult },
                             second_dart: Dart { value: second_dart_value, multiplier: second_dart_mult }, third_dart: Dart { value: third_dart_value, multiplier: 2 } } )
                     }
@@ -215,59 +197,29 @@ fn main() {
             }
         }
     }
-    for c in &checkouts {
-        println!("match {}*{}+ {}*{}+ 2*{}",
-                 c.first_dart.multiplier,c.first_dart.value,c.second_dart.multiplier,c.second_dart.value, c.third_dart.value);
-
-    }
-
-
 
     let mut halfablemap = HashMap::<usize,u32>::new();
     for (i,h) in HALFABLE.iter().enumerate() {
-        //println!(" key: {} value: {}",i,h);
 
         if i==20 { halfablemap.insert(25,0); continue}
         halfablemap.insert(i+1,*h);
     }
-//
-   //     println!("v {v}");
-  //  }
-    for c in &halfablemap {
-        println!(" key: {} value: {}",c.0,c.1);
 
-    }
     checkouts.sort_by(|a,b| halfablemap[&(b.third_dart.value as usize)].cmp(&halfablemap[&(a.third_dart.value as usize)]));
-    for mut c in &mut checkouts {
+    for c in &mut checkouts {
         if c.second_dart.multiplier==0 && c.first_dart.multiplier >0
         {
             swap(&mut c.first_dart,&mut c.second_dart);
         }
     }
 
-
-
-
-     checkouts.sort_by(|a,b| a.first_dart.multiplier.cmp(&b.first_dart.multiplier));
-     checkouts.sort_by(|a,b| a.second_dart.multiplier.cmp(&b.second_dart.multiplier));
 checkouts.sort();
     checkouts.dedup();
+    checkouts.sort_by(|a,b| a.first_dart.multiplier.cmp(&b.first_dart.multiplier));
+    checkouts.sort_by(|a,b| a.second_dart.multiplier.cmp(&b.second_dart.multiplier));
 
     for c in &checkouts {
-        if  c.first_dart.multiplier>0 && c.second_dart.multiplier>0
-        {
-            println!("match {}*{}+ {}*{}+ 2*{} -> Score {:?}",
-                     c.first_dart.multiplier, c.first_dart.value, c.second_dart.multiplier, c.second_dart.value, c.third_dart.value, c.complexity());
-        }else if c.first_dart.multiplier==0 && c.second_dart.multiplier>0
-        {
-            println!("match      {}*{}+ 2*{} -> Score {:?}",
-                    c.second_dart.multiplier, c.second_dart.value, c.third_dart.value, c.complexity());
-        }else  {
-            println!("match           2*{} -> Score {:?}",
-                           c.third_dart.value,  c.complexity());
-        }
-
+        println!("{}",c);
     }
     println!("Found {} matches",checkouts.len());
-
 }
